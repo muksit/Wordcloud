8ac1e812f77050bff97a4fbc7812e0730455999e
Muksit <muksit@gmail.com>
2014-09-10 17:42:41 -0400

    added support for local testing

--- twitterapp.py
+++ twitterapp.py
@@ -13,10 +13,12 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config["DEBUG"] = True  # True only when testing
 
-if (os.environ['twitkey']):
-    api = TwitterAPI(os.environ['twitkey'], os.environ['twitsecret'], os.environ['twittoken'], os.environ['twittoken_secret'])
-else:
-    api = TwitterAPI(appconfig.apikeys['key'], appconfig.apikeys['secret'], appconfig.apikeys['token'], appconfig.apikeys['token_secret'])
+#for Heroku
+api = TwitterAPI(os.environ['twitkey'], os.environ['twitsecret'], os.environ['twittoken'], os.environ['twittoken_secret'])
+
+#for testing locally
+#api = TwitterAPI(appconfig.apikeys['key'], appconfig.apikeys['secret'], appconfig.apikeys['token'], appconfig.apikeys['token_secret'])
+
 blacklist = "a,we,We,table,It,Our,us,us.,/,/,The,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your".split(",")
 
 wordtograph = ""
